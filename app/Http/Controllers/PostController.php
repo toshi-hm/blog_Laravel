530397d483f1e24d52e615fb99db20aefc6589a4
toshi-hm <toshiyahama0310@gmail.com>
2022-10-23 15:39:44 +0900

    投稿一覧を表示できるようにした

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -18,8 +18,7 @@ class PostController extends Controller
  // インポートしたPostをインスタンス化して$postとして使用
  public function index(Post $post)
  {
-     // $postの中身を戻り値にする
-     return $post -> get();
+  return view("posts/index") -> with(["posts" => $post -> get()]);
  }
  
 }
