065ea5bbe7e0c4e46421558596e66aeadc4035ac
toshi-hm <toshiyahama0310@gmail.com>
2022-10-24 05:16:05 +0900

    投稿したものをデータベースに登録できるようにした

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -28,4 +28,11 @@ class PostController extends Controller
  {
   return view("posts/show") -> with(["post" => $post]);
  }
+ public function store(Request $request, Post $post)
+ {
+  // dd($request->all());
+  $input = $request["post"];
+  $post->fill($input)->save();
+  return redirect("/posts/" . $post->id);
+ }
 }
